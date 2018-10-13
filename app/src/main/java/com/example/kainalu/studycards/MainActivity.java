f1c72e6e99cee3a86f598a9d3b12816cd277678f
person808 <kainaluh808@gmail.com>
2018-10-13 13:54:38 -0700

    Initial commit.

---
+++ app/src/main/java/com/example/kainalu/studycards/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.kainalu.studycards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView questionView;
+    private TextView answerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        questionView = findViewById(R.id.flashcard_question);
+        answerView = findViewById(R.id.flashcard_answer);
+
+        questionView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                questionView.setVisibility(View.INVISIBLE);
+                answerView.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
