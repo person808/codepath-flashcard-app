4379b3c195aae33aacad6013d3cede21ebe0e330
person808 <kainaluh808@gmail.com>
2018-11-26 15:45:03 -0800

    Lab 4 required stories.

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 ## Study Cards 
 
-## Lab 3
-
 ### App Description
 A simple flashcard app to study with.
 
+## Lab 4
+
 ### App Walk-though
 `TODO://` Add the URL to your animated app walk-though `gif` in the image tag below. Make sure the gif actually renders and animates when viewing this README. (☝️ Remove this paragraph after after adding gif)
+
+<img src="lab4.gif" width=200><br>
+
+
+## Required
+- [x] User sees a transition animation when going from the main screen to the add card screen.
+- [x] User sees a reveal animation when the flashcard is tapped and the answer is shown
+- [x] User sees flashcards animating in / out when the 'next' button is tapped
+
+## Optional
+- [ ] Extra styling is added!
+- [ ] User sees a countdown timer for each question
+- [ ] User sees a 'card flip' animation when the card is tapped
+- [ ] User sees a celebration animation with confetti when they answer a question correct
+
+## Lab 3
+
+### App Walk-though
 
 <img src="lab3.gif" width=200><br>
 
--- app/src/main/java/com/example/kainalu/studycards/MainActivity.java
+++ app/src/main/java/com/example/kainalu/studycards/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.kainalu.studycards;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -43,8 +47,22 @@ public class MainActivity extends AppCompatActivity {
         questionView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // get the center for the clipping circle
+                int cx = answerView.getWidth() / 2;
+                int cy = answerView.getHeight() / 2;
+
+                // get the final radius for the clipping circle
+                float finalRadius = (float) Math.hypot(cx, cy);
+
+                // create the animator for this view (the start radius is zero)
+                Animator anim = ViewAnimationUtils.createCircularReveal(answerView, cx, cy, 0f, finalRadius);
+
+                // hide the question and show the answer to prepare for playing the animation!
                 questionView.setVisibility(View.INVISIBLE);
                 answerView.setVisibility(View.VISIBLE);
+
+                anim.setDuration(300);
+                anim.start();
             }
         });
 
@@ -61,30 +79,50 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, NewCardActivity.class);
                 MainActivity.this.startActivityForResult(intent, 100);
+                overridePendingTransition(R.anim.right_in, R.anim.left_out);
             }
         });
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // advance our pointer index so we can show the next card
-                currentCardDisplayedIndex++;
+                final Animation leftOutAnim = AnimationUtils.loadAnimation(v.getContext(), R.anim.left_out);
+                final Animation rightInAnim = AnimationUtils.loadAnimation(v.getContext(), R.anim.right_in);
 
-                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
-                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
-                    currentCardDisplayedIndex = 0;
-                }
+                leftOutAnim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        // advance our pointer index so we can show the next card
+                        currentCardDisplayedIndex++;
 
-                // set the question and answer TextViews with data from the database
-                questionView.setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
-                answerView.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                        // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                        if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                            currentCardDisplayedIndex = 0;
+                        }
 
-                // make sure question is showing
-                answerView.setVisibility(View.INVISIBLE);
-                questionView.setVisibility(View.VISIBLE);
+                        // make sure question is showing
+                        questionView.setVisibility(View.VISIBLE);
+                        answerView.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        // set the question and answer TextViews with data from the database
+                        questionView.setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                        answerView.setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                        questionView.startAnimation(rightInAnim);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+                questionView.startAnimation(leftOutAnim);
             }
-    });
-}
+        });
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
